8019bdd11bb80496b28e511d019f642cc3e5ef48
johnson <1392374482@qq.com>
2019-11-08 21:37:06 +0800

    2019_11_8

---
+++ Desktop/Git/Project78/Project78/源.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+void Swap(int&a, int&b)
+{
+	int temp = a;
+	a = b;
+	b = temp;
+}
+
+
+void _quickSort(vector<int>& v)
+{
+	int left, right;
+}
+
+
+void QuickSort(vector<int>& v)
+{
+	
+}
+
+int main()
+{
+	vector<int> v{ 4, 5, 6, 2, 1, 9, 8, 7, 3 };
+
+	return 0;
+}
